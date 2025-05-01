e09874d13c71abb63349013a41dbaae22ead1a41
PavanCodes05 <pavancodes05@gmail.com>
2025-05-01 20:29:49 +0530

    Modify Arrays/Logic Building/removeDuplicatesFromSortedArray

--- Arrays/Logic Building/removeDuplicatesFromSortedArray.py
+++ Arrays/Logic Building/removeDuplicatesFromSortedArray.py
@@ -12,7 +12,8 @@ If the number of unique elements be k, then,
 *   The remaining elements, as well as the size of the array does not matter in terms of
     correctness.
 
-An array sorted in non-decreasing order is an array where every element to the right of an element is either equal to or greater in value than that element.
+An array sorted in non-decreasing order is an array where every element to the 
+right of an element is either equal to or greater in value than that element.
 
 TC:
 1.  Input: nums = [0, 0, 3, 3, 5, 6]
@@ -31,32 +32,41 @@ TC:
 nums = [0, 0, 3, 3, 5, 6]
 
 def removeDuplicatesBrute(nums):
+    # Keep track of added elements
     seen = set()
+    # Temp array to store only unique elements
     temp = []
 
+    # Traverse through the array to add unique elements to temp[] 
     for num in nums:
         if num not in seen:
             temp.append(num)
             seen.add(num)
 
+    # Modify the first 'k' values of actual array
     for i in range(len(temp)):
         nums[i] = temp[i]
 
+    # We have return the number of k elements
     return len(temp)
 
 # Optimal Solution
 
 def removeDuplicatesOptimal(nums):
+    # If the array is empty, which means there no unique elements or duplicates
     if not nums:
         return 0
-
+    # We know that the first element is perfect: Sorted, not a duplicate 
     i = 0
 
+    # We look for the next element to be placed after i
     for j in range(1, len(nums)):
+        # We find a non-equal element to i
         if nums[i] != nums[j]:
+            # Place it right after i 
             i += 1
             nums[i] = nums[j]
-    
+    # Current pos of 'i' has the number of unique elements, since 'i' starts from 0 we add 1 and return
     return i + 1
 
 count_unique = removeDuplicatesOptimal(nums)
@@ -85,13 +95,17 @@ Approach:
     We have to find a way to not to use extra space to store the result
 
     Optimal: The optimal solution is very similar to the last problem we solved,
-    In last problem we had to move all the zeros to the end, now we have to move all the
-    duplicates to somewhere, we doesn't have to explicitly move it to the end but it is possible,
-    We use two pointers, 'i' being with the first element, because we know that element is already perfect
-    if we have only one element that is going to be perfect, so we keep the other pointer on the next element
-    and we move the right pointer till we find a non-equal value of the first pointer, once we find that we move the 'i' pointer and assign the non-equal value to the current 'i' position, now we have the perfect
-    values alligned in the same order but avoiding duplicates, now we can finally return the 'i + 1' value, because the final value inserted in the i-th position will be the number of unique elements and as i starts 
-    from 0 we have to add 1 to it and return it.
+    In last problem we had to move all the zeros to the end, now we have to move 
+    all the duplicates to somewhere, we doesn't have to explicitly move it to the 
+    end but it is possible, We use two pointers, 'i' being with the first element, 
+    because we know that element is already perfect,if we have only one element that 
+    is going to be perfect, so we keep the other pointer on the next element and we 
+    move the right pointer till we find a non-equal value of the first pointer, once 
+    we find that we move the 'i' pointer and assign the non-equal value to the current 
+    'i' position, now we have the perfect values alligned in the same order but avoiding 
+    duplicates, now we can finally return the 'i + 1' value, because the final value inserted 
+    in the i-th position will be the number of unique elements and as i starts from 0 
+    we have to add 1 to it and return it.
 
     Time: O(n)
     Space: O(1) -> Everything is done in-place
